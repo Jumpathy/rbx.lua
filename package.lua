da5857ff86e368494699e17f40a122c33e2570b2
iiToxicity <52023520+iiToxicity@users.noreply.github.com>
2020-10-25 19:31:16 -0500

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,14 @@
+-- DO NOT DELETE
+
+return {
+	name = "iiToxicity/rbx.lua",
+    version = "1.0.1";
+	dependencies = {
+		"creationix/coro-http@3.1.0",
+		"creationix/coro-websocket@3.1.0",
+		"luvit/secure-socket@1.2.2",
+	},
+	tags = {};
+	author = "iiToxicity",
+	files = {"**.lua"},
+}
